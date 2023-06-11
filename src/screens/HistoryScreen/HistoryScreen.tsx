d0448516a459600f6420d543107fa3f36c1b56d5
EnotelloR <uncleegor05@gmail.com>
2023-06-11 15:32:03 +0300

    HistoryScreen.tsx fix

--- src/screens/HistoryScreen/HistoryScreen.tsx
+++ src/screens/HistoryScreen/HistoryScreen.tsx
@@ -5,5 +5,5 @@ import { usePreference } from '@features/profile';
 export const HistoryScreen = () => {
   const preference = usePreference();
 
-  return preference.data?.id && <History carID={preference.data.id} />;
+  return preference.data?.id ? <History carID={preference.data.id} /> : null;
 };
